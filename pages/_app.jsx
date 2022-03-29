ddf9e3ca2d7f389e9332d02f4ec7e37dfee09204
alexquenelle <alexandre.quenelle@epitech.eu>
2022-03-29 12:29:35 +0200

    front/add_go_back_footer [ADD] go back footer button + metrial ui on the add song pages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,18 @@
 import NavBar from './navbar';
+import { useRouter } from 'next/router';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Button } from '@mui/material';
 
 function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
     return (
         <>
             <NavBar />
             <Component {...pageProps} />
+            <Button onClick={() => router.back()} variant="contained" startIcon={<ArrowBackIosIcon />}>
+                Go Back
+            </Button>
         </>
     );
 }
